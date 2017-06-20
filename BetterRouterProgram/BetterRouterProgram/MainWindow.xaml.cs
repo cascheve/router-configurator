e5a8b326845ecbeccdf03470442a3ee28de4731d
Ryan Mahoney <QWVB64@motorolasolutions.com>
2017-06-20 15:37:53 -0500

    regroup1

--- BetterRouterProgram/BetterRouterProgram/MainWindow.xaml.cs
+++ BetterRouterProgram/BetterRouterProgram/MainWindow.xaml.cs
@@ -1,30 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.IO.Ports;
 
 namespace BetterRouterProgram
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
-            PortChat.Run(this);
+
+            this.portNameDD.Background = Brushes.LightGray;
+            this.portNameDD.MaxWidth = (this.Width) / 7;
+            this.portNameDD.MaxHeight = (this.Height) / 8;
+
+            FillPortNames(this);
+        }
+
+        public static void FillPortNames(MainWindow m)
+        {
+
+            //Console.WriteLine("Available Ports:");
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                ComboBoxItem cBoxItem = new ComboBoxItem();
+                cBoxItem.Content = s;
+                m.portNameDD.Items.Add(cBoxItem);
+            }
+
+        }
+
+        private void attemptConnection(object sender, RoutedEventArgs e)
+        {
+            string comPort = this.portNameDD.Text;
+            string uString = this.username.Text;
+            string pString = this.password.Text;
+
+
         }
     }
 }
--- BetterRouterProgram/BetterRouterProgram/PortChat.cs
+++ BetterRouterProgram/BetterRouterProgram/PortChat.cs
@@ -5,8 +5,6 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.IO.Ports;
-using System.Windows.Controls;
-using System.Windows.Media;
 
 namespace BetterRouterProgram
 {
@@ -14,17 +12,10 @@ namespace BetterRouterProgram
     {
         static bool moveOn;
         static SerialPort serialPort;
-        static ComboBox portNameDD;
 
         public static void Run(MainWindow m)
         {
-            portNameDD = m.portNameDD as ComboBox;
-            portNameDD.Background = Brushes.LightGray;
-            portNameDD.MaxWidth = (m.Width) / 7;
-            portNameDD.MaxHeight = (m.Height) / 8;
-
-            FillPortNames();
-
+            
             StringComparer stringComparer = StringComparer.OrdinalIgnoreCase;
             Thread readThread = new Thread(Read);
 
@@ -60,20 +51,6 @@ namespace BetterRouterProgram
 
                 }
             }
-        }
-
-        // Display Port values and prompt user to enter a port.
-        public static void FillPortNames()
-        {
-
-            //Console.WriteLine("Available Ports:");
-            foreach (string s in SerialPort.GetPortNames())
-            {
-                ComboBoxItem cBoxItem = new ComboBoxItem();
-                cBoxItem.Content = s;
-                portNameDD.Items.Add(cBoxItem);
-            }
-
-        }
+        }     
     }
 }
